1bf72a26ed842ae094a14b8b3628cee417ed3a71
Мария <1481047@mail.ru>
2021-12-15 16:21:50 +0300

    Добавьте файлы проекта.

---
+++ VS 23 var2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VS_23_var2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FactorialAsync();
+            Proverka(10);
+            Console.WriteLine("Финал");
+            Console.ReadKey();
+        }
+        static int Proverka(int n)
+        {
+            Console.WriteLine("Начало проверки");            
+            int m = 1;
+            for (int i = 1; i < n + 1; i++)
+            {
+                m *= i;
+                Console.WriteLine($"результат проверки: {m}");
+            }
+            Console.WriteLine("Конец проверки");
+            return m;            
+        }
+        static int Factorial(int x)
+        {
+            int n = 0;
+            if (x == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                n = x * Factorial(x - 1);
+                Console.WriteLine($"результат факториала: {n}");
+                return n;
+            }
+        }
+        static async Task FactorialAsync()
+        {
+            Console.WriteLine("Вычислить факториал числа. Ввести целое число:");            
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Начало метода факториал");
+            await Task.Run(() => Factorial(n));
+            Console.WriteLine("Финал метода факториал");            
+        }
+    }
+}
+    
+
---
+++ VS 23.1.1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VS_23._1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int r = FactorialAsync().Result;              
+            Proverka();
+            Console.WriteLine("Финал");
+            Console.ReadKey();
+        }
+        static void Proverka()
+        {
+            Console.WriteLine("\nДля проверки ввести целое число");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int m = 1;
+            for (int i = 1; i < n + 1; i++)
+            {
+                m *= i;
+                Console.WriteLine($"результат: {m}");
+            }
+            Console.WriteLine($"Итого {m}");
+        }
+        static int Factorial(int x)
+        {
+            int n = 0;
+            if (x == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                n = x * Factorial(x - 1);
+                Console.WriteLine($"результат: {n}");
+                return n;
+            }
+
+        }
+        static async Task<int> FactorialAsync()
+        {
+            Console.WriteLine("Вычислить факториал числа. Ввести целое число:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int result = await Task.Run(() => Factorial(n));
+            Console.WriteLine($"Итого: {result}");
+            return result;
+        }
+    }
+}
